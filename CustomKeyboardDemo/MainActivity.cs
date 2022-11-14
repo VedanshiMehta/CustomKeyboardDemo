9b174ec08170d9acdd67918841ee60048c6da1fc
vedanshi.mehta <vedanshi.mehta@1R-79>
2022-11-14 17:39:34 +0530

    Add project files.

---
+++ CustomKeyboardDemo/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.InputMethodServices;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using Java.Lang;
+
+namespace CustomKeyboardDemo
+{
+    //[Service(Permission = "android.permission.BIND_INPUT_METHOD", Label = "CUSTOMKEYBOARD", Exported = true, Enabled = true)]
+    //[MetaData("android.view.im", Resource = "@xml/Method")]
+    //[IntentFilter(new string[] { "android.view.InputMethod" })]
+    [System.Obsolete]
+    public class MainActivity : InputMethodService, KeyboardView.IOnKeyboardActionListener
+    {
+        private KeyboardView _keyboardView;
+        private Keyboard _keyboard;
+        private bool isCaps = false;
+
+        public override View OnCreateInputView()
+        {
+            //_keyboardView = (KeyboardView)LayoutInflater.Inflate(Resource.Layout.activity_main, null);
+            //_keyboard = new Keyboard(this, Resource.Xml.Qwerty);
+            //_keyboardView.Keyboard = _keyboard;
+            //_keyboardView.OnKeyboardActionListener = this;
+
+            //return _keyboardView;
+            return null;
+        }
+        public void OnKey([GeneratedEnum] Android.Views.Keycode primaryCode, [GeneratedEnum] Android.Views.Keycode[] keyCodes)
+        {
+            
+        }
+
+        public void OnPress([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+     
+        }
+
+        public void OnRelease([GeneratedEnum] Android.Views.Keycode primaryCode)
+        {
+        
+        }
+
+        public void OnText(ICharSequence text)
+        {
+            
+        }
+
+        public void SwipeDown()
+        {
+           
+        }
+
+        public void SwipeLeft()
+        {
+           
+        }
+
+        public void SwipeRight()
+        {
+           
+        }
+
+        public void SwipeUp()
+        {
+            
+        }
+    }
+}
